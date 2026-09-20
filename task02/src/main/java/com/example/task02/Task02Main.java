23296920c7ec23bfe175ff155c7600a928bef888
Egor.Bigildeev <GeoBig74@gmail.com>
2026-09-20 14:22:45 +0500

    Module 3

--- task02/src/main/java/com/example/task02/Task02Main.java
+++ task02/src/main/java/com/example/task02/Task02Main.java
@@ -4,12 +4,14 @@ public class Task02Main {
     public static void main(String[] args) {
         //здесь вы можете вручную протестировать ваше решение, вызывая реализуемый метод и смотря результат
         // например вот так:
-        /*
+
         System.out.println(getSeason(12));
-         */
+
     }
 
     static String getSeason(int monthNumber) {
-        return "";//todo напишите здесь свою корректную реализацию этого метода, вместо существующей
+        String[] seasons = {"зима", "весна", "лето", "осень"};
+        int s = monthNumber % 12;
+        return seasons[s / 3];
     }
 }
